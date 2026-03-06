cc285a9dc3991d5c75a44307ca5406051885ef10
727 <39704214+852764@users.noreply.github.com>
2026-03-06 11:24:00 +0800

    Update KernelSU repository URL in setup script

--- setup.sh
+++ setup.sh
@@ -39,7 +39,7 @@ perform_cleanup() {
 # Sets up or update KernelSU environment
 setup_kernelsu() {
     echo "[+] Setting up KernelSU..."
-    test -d "$GKI_ROOT/KernelSU" || git clone https://github.com/852764/KernelSU && echo "[+] Repository cloned."
+    test -d "$GKI_ROOT/KernelSU" || git clone https://github.com/tiann/KernelSU/tree/v0.7.0 && echo "[+] Repository cloned."
     cd "$GKI_ROOT/KernelSU"
     git stash && echo "[-] Stashed current changes."
     if [ "$(git status | grep -Po 'v\d+(\.\d+)*' | head -n1)" ]; then
